6f1c31f49cca0a9cc30b5f06fa8e9b8e4dcc6b42
Shreyas Minocha <sm-git@riseup.net>
2020-04-13 19:22:02 +0530

    Add more prefs and metadata to the json files

--- librewolf.cfg/controversial.js
+++
@@ -1,5 +0,0 @@
-//lockPref("dom.indexedDB.logging.details", false);
-//lockPref("dom.indexedDB.logging.enabled", false);
-//lockPref("network.http.spdy.enabled", false);
-//lockPref("network.http.spdy.enabled.deps", false);
-//lockPref("network.http.spdy.enabled.http2", false);
--- librewolf.cfg/defaulting.js
+++
@@ -1,4 +0,0 @@
-//defaultPref("intl.accept_languages", "en-US, en"); //This make lang windows unusable
-//defaultPref("network.http.sendRefererHeader", 1);
-//defaultPref("extensions.ui.dictionary.hidden", false);
-//defaultPref("extensions.ui.locale.hidden", false);
